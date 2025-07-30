5f39e3837b362ea0471fd3f6c03c2d1fec0d6d0d
Daniel Nzekwe <nzekwe.da@wcs-g.com>
2025-07-30 03:21:46 +0000

    Experimenting with doxygen.. not final docs

---
+++ docs/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['contribution_0',['Contribution',['../md_README.html#autotoc_md4',1,'']]]
+];
--- include/scratch/field.h
+++ include/scratch/field.h
@@ -3,8 +3,24 @@
 #pragma once
 #include <memory>
 #include <string>
+#include <vector>
 
-namespace libscratch
-{
-  class Field;
+namespace scratch {
+/**
+ * @brief Represents a field in a Scratch block.
+ *
+ * A field is a piece of data associated with a block, such as a label or input value.
+ */
+class Field {
+    public:
+        Field(const std::string& name, const std::string& type)
+            : name(name), type(type) {}
+
+        std::string getName() const { return name; }
+        std::string getType() const { return type; }
+
+    private:
+        std::string name;
+        std::string type;
+};
 }
